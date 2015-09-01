2bcfb020623eb8ad3969be21b8b54b973a7af627
Peter Korduan <peter.korduan@gdi-service.de>
2015-09-01 20:19:58 +0200

    Add install of phpmyadmin into docker image. Create pmacontroluser. Add config.inc.php as source and mount it to etc/phpmyadmin.

--- etc/phpmyadmin/config.inc.php
+++ etc/phpmyadmin/config.inc.php
@@ -19,7 +19,7 @@ $cfg['blowfish_secret'] = "blowfish_secret"; /* YOU MUST FILL IN THIS FOR COOKIE
 /*
  * Absolute URI
  */
-$cfg['PmaAbsoluteUri'] = "$PMA_URI";
+$cfg['PmaAbsoluteUri'] = 'http://' . getenv('IP_EXTERN') . '/userDbAdmin/';
 
 /*
  * Servers configuration
