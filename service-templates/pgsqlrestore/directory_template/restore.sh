abb7929d86eb78ac4d65ee766bbaf32a238d0a43
Peter Korduan <peter.korduan@gdi-service.de>
2023-09-20 14:41:04 +0200

    pgsqlrestore: restore.sh nimmt Label einer Sicherung als Parameter entgegen

--- service-templates/pgsqlrestore/directory_template/restore.sh
+++ service-templates/pgsqlrestore/directory_template/restore.sh
@@ -5,4 +5,4 @@ docker run --rm -it \
 -v $(pwd)/data:/var/lib/postgresql/data \
 -v $(pwd)/backup:/pgbackrest \
 pkorduan/postgis:15-3.3 \
-pgbackrest --stanza=local --log-level-console=detail --set= --type=time --target=2015-01-30 14:15:11 EST restore
+pgbackrest --stanza=local --log-level-console=detail --set=${1} --type=immediate --target-action=promote --archive-mode=off restore
