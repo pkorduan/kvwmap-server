506956df78068a4f8e3b7c0c87a47247774691ac
Peter Korduan <peter.korduan@gdi-service.de>
2023-10-18 08:12:01 +0200

    SSL-Extend script

--- service-templates/proxy/init-letsencrypt.sh
+++ service-templates/proxy/init-letsencrypt.sh
@@ -1,93 +1,14 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
+www=/var/www/html
+letsencrypt=/etc/letsencrypt
+log=/var/log/letsencrypt
+network=/home/gisadmin/networks/proxy/services/proxy
+email=peter.korduan@gdi-service.de
 
-SERVICE_PATH=/home/gisadmin/networks/proxy/services
-
-domains=(platzhalterkvwmapserverdomainname)
-rsa_key_size=4096
-data_path="./letsencrypt"
-email="peter.korduan@gdi-service.de" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
-
-echo "Ersetze die Platzhalter für den Servernamen durch ${HOSTNAME} in der nginx Konfiguration."
-sed -i -e "s|platzhalterkvwmapserverdomainname|${HOSTNAME}|g" ${SERVICE_PATH}/proxy/init-letsencrypt.sh
-sed -i -e "s|platzhalterkvwmapserverdomainname|${HOSTNAME}|g" ${SERVICE_PATH}/proxy/nginx/sites-available/default.conf
-sed -i -e "s|platzhalterkvwmapserverdomainname|${HOSTNAME}|g" ${SERVICE_PATH}/proxy/nginx/sites-available/default-ssl.conf
-
-echo "Stelle auf HTTPS um in der nginx Konfiguration."
-sed -i '/default.conf;/a\    include /etc/nginx/default-ssl.conf;' ${SERVICE_PATH}/proxy/nginx/sites-available/nginx.conf
-
-
-if [ ! -e "$data_path/options-ssl-nginx.conf" ] || [ ! -e "$data_path/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/ssl-dhparams.pem"
-  echo
-fi
-
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
-
-
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
-echo
-
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
-
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
-
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
-
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
-
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --force-renewal --server https://acme-v02.api.letsencrypt.org/directory \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-
-#docker run --rm --name certbot -v "/home/gisadmin/proxy/www:/var/www" -v "/home/gisadmin/proxy/letsencrypt:/etc/letsencrypt" -v "/home/gisadmin/proxy/log:/var/log/letsencrypt" certbot/certbot certonly
-
-echo
-
-echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker run -it --rm --name certbot \
+           -v "$network/www/html:$www" \
+           -v "$network/letsencrypt/:$letsencrypt" \
+           -v "$network/log:$log" \
+           certbot/certbot certonly --webroot -w $www -d $1 --email $email --non-interactive --agree-tos
+dcm proxy reload
